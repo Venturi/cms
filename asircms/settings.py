d4b48717a6144d7cbca47d93f0ad2dd790ac2842
Manuel "Venturi" Porras Peralta <venturi@openmailbox.org>
2015-12-21 20:15:49 +0100

    Progresos con People y Newblogs

--- asircms/settings.py
+++ asircms/settings.py
@@ -47,7 +47,7 @@ WSGI_APPLICATION = 'asircms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
+	
 
 
 # Internationalization
@@ -73,163 +73,178 @@ MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'asircms', 'static'),
+	os.path.join(BASE_DIR, 'asircms', 'static'),
 )
 SITE_ID = 1
 
+STATICFILES_FINDERS = [
+					'django.contrib.staticfiles.finders.FileSystemFinder',
+					# important! place right before django.contrib.staticfiles.finders.AppDirectoriesFinder
+					'aldryn_boilerplates.staticfile_finders.AppDirectoriesFinder',
+					'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+					]
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'asircms', 'templates'),],
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'django.core.context_processors.media',
-    'django.core.context_processors.csrf',
-    'django.core.context_processors.tz',
-    'sekizai.context_processors.sekizai',
-    'django.core.context_processors.static',
-    'cms.context_processors.cms_settings',
-    'aldryn_boilerplates.context_processors.boilerplate',
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-    'aldryn_boilerplates.template_loaders.AppDirectoriesLoader',
-    'django.template.loaders.app_directories.Loader',
-    'django.template.loaders.eggs.Loader'
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'asircms', 'templates'),],
+		'OPTIONS': {
+			'context_processors': [
+				'django.contrib.auth.context_processors.auth',
+	'django.contrib.messages.context_processors.messages',
+	'django.core.context_processors.i18n',
+	'django.core.context_processors.debug',
+	'django.core.context_processors.request',
+	'django.core.context_processors.media',
+	'django.core.context_processors.csrf',
+	'django.core.context_processors.tz',
+	'sekizai.context_processors.sekizai',
+	'django.core.context_processors.static',
+	'cms.context_processors.cms_settings',
+	'aldryn_boilerplates.context_processors.boilerplate',
+			],
+			'loaders': [
+				'django.template.loaders.filesystem.Loader',
+	'aldryn_boilerplates.template_loaders.AppDirectoriesLoader',
+	'django.template.loaders.app_directories.Loader',
+	'django.template.loaders.eggs.Loader'
+			],
+		},
+	},
 ]
 
 
 MIDDLEWARE_CLASSES = (
-    'cms.middleware.utils.ApphookReloadMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'cms.middleware.user.CurrentUserMiddleware',
-    'cms.middleware.page.CurrentPageMiddleware',
-    'cms.middleware.toolbar.ToolbarMiddleware',
-    'cms.middleware.language.LanguageCookieMiddleware'
+	'cms.middleware.utils.ApphookReloadMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'cms.middleware.user.CurrentUserMiddleware',
+	'cms.middleware.page.CurrentPageMiddleware',
+	'cms.middleware.toolbar.ToolbarMiddleware',
+	'cms.middleware.language.LanguageCookieMiddleware'
 )
 
 INSTALLED_APPS = (
-    'djangocms_admin_style',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.admin',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
-    'cms',
-    'menus',
-    'sekizai',
-    'treebeard',
-    'djangocms_text_ckeditor',
-    'djangocms_style',
-    'djangocms_column',
-    'djangocms_file',
-    'djangocms_flash',
-    'djangocms_googlemap',
-    'djangocms_inherit',
-    'djangocms_link',
-    'djangocms_picture',
-    'djangocms_teaser',
-    'djangocms_video',
-    'reversion',
-    'asircms',
+	'djangocms_admin_style',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.admin',
+	'django.contrib.sites',
+	'django.contrib.sitemaps',
+	'django.contrib.staticfiles',
+	'django.contrib.messages',
+	'cms',
+	'menus',
+	'sekizai',
+	'treebeard',
+	'djangocms_text_ckeditor',
+	'djangocms_style',
+	'djangocms_column',
+	'djangocms_file',
+	'djangocms_flash',
+	'djangocms_googlemap',
+	'djangocms_inherit',
+	'djangocms_link',
+	'djangocms_picture',
+	'djangocms_teaser',
+	'djangocms_video',
+	'reversion',
 
-'aldryn_apphooks_config',
-'aldryn_boilerplates',
-'aldryn_categories',
-'aldryn_common',
-'aldryn_newsblog',
-'aldryn_people',
-'aldryn_reversion',
-'aldryn_translation_tools',
-'easy_thumbnails',
-'filer',
-'parler',
-'sortedm2m',
-'taggit',
+	'aldryn_apphooks_config',
+	'aldryn_boilerplates',
+	'aldryn_categories',
+	'aldryn_common',
+	'aldryn_newsblog',
+	'aldryn_people',
+	'aldryn_reversion',
+	'aldryn_translation_tools',
+	'easy_thumbnails',
+	'filer',
+	'parler',
+	'sortedm2m',
+	'taggit',
 
+	'asircms',
 )
 
 THUMBNAIL_PROCESSORS = (
-    'easy_thumbnails.processors.colorspace',
-    'easy_thumbnails.processors.autocrop',
-    # 'easy_thumbnails.processors.scale_and_crop',
-    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
-    'easy_thumbnails.processors.filters',
-    'easy_thumbnails.processors.background',
+	'easy_thumbnails.processors.colorspace',
+	'easy_thumbnails.processors.autocrop',
+	# 'easy_thumbnails.processors.scale_and_crop',
+	'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+	'easy_thumbnails.processors.filters',
+	'easy_thumbnails.processors.background',
 )
 
 LANGUAGES = (
-    ## Customize this
-    ('es', gettext('es')),
-    ('en', gettext('en')),
+	## Customize this
+	('es', gettext('es')),
+	('en', gettext('en')),
 )
 
 CMS_LANGUAGES = {
-    ## Customize this
-    'default': {
-        'public': True,
-        'hide_untranslated': False,
-        'redirect_on_fallback': True,
-    },
-    1: [
-        {
-            'public': True,
-            'code': 'es',
-            'hide_untranslated': False,
-            'name': gettext('es'),
-            'redirect_on_fallback': True,
-        },
-        {
-            'public': True,
-            'code': 'en',
-            'hide_untranslated': False,
-            'name': gettext('en'),
-            'redirect_on_fallback': True,
-        },
-    ],
+	## Customize this
+	'default': {
+		'public': True,
+		'hide_untranslated': False,
+		'redirect_on_fallback': True,
+	},
+	1: [
+		{
+			'public': True,
+			'code': 'es',
+			'hide_untranslated': False,
+			'name': gettext('es'),
+			'redirect_on_fallback': True,
+		},
+		{
+			'public': True,
+			'code': 'en',
+			'hide_untranslated': False,
+			'name': gettext('en'),
+			'redirect_on_fallback': True,
+		},
+	],
 }
 
 CMS_TEMPLATES = (
-    ## Customize this
-    ('page.html', 'Page'),
-    ('feature.html', 'Page with Feature')
+	## Customize this
+	('page.html', 'Page'),
+	('feature.html', 'Page with Feature')
 )
 
-ALDRYN_BOILERPLATE_NAME='bootstrap3'
+ALDRYN_BOILERPLATE_NAME='legacy'
 
 CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': 'localhost',
-        'NAME': 'project.db',
-        'PASSWORD': '',
-        'PORT': '',
-        'USER': ''
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'HOST': 'localhost',
+		'NAME': 'project.db',
+		'PASSWORD': '',
+		'PORT': '',
+		'USER': ''
+	}
 }
 
 MIGRATION_MODULES = {
-    
+	'djangocms_picture': 'djangocms_picture.migrations_django',
+	'djangocms_file': 'djangocms_file.migrations_django',
+	'djangocms_googlemap': 'djangocms_googlemap.migrations_django',
+	'djangocms_teaser': 'djangocms_teaser.migrations_django',
+	'djangocms_style': 'djangocms_style.migrations_django',
+	'djangocms_flash': 'djangocms_flash.migrations_django',
+	'djangocms_video': 'djangocms_video.migrations_django',
+	'djangocms_column': 'djangocms_column.migrations_django',
+	'djangocms_inherit': 'djangocms_inherit.migrations_django',
+	#'aldryn_people': 'aldryn_people.south_migrations',
 }
